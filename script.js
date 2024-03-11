22bb83b7d858f59c0716f9225d2061a3621f9917
Federico Torres <federico.torres@autodesk.com>
2024-03-10 22:18:54 -0600

    basic styles

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function redirectToPortfolio() {
+    window.location.href = "https://www.example.com/portfolio";
+}
+
+function redirectToBlog() {
+    window.location.href = "https://fedetorresdev.com";
+}
