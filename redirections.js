57cf95176dd9e88d1cdb2d1b008b69086dbe5d10
Federico Torres <federico.torres@autodesk.com>
2024-03-10 23:46:25 -0600

    added contact page

---
+++ redirections.js
@@ -0,0 +1,8 @@
+
+function redirectToBlog() {
+    window.location.href = "https://fedetorresdev.com";
+}
+
+function redirectToContact() {
+    window.location.href = "contact.html"
+}
--- script.js
+++
@@ -1,8 +0,0 @@
-
-function redirectToPortfolio() {
-    window.location.href = "https://www.example.com/portfolio";
-}
-
-function redirectToBlog() {
-    window.location.href = "https://fedetorresdev.com";
-}
